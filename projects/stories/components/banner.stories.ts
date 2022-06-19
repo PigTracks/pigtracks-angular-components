a2988a6b439b47a97d12d7187e7c66cbf1c03433
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2022-06-19 11:46:28 -0500

    Add storybook

---
+++ projects/stories/components/banner.stories.ts
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/angular';
+import { BannerComponent } from '@nesso/angular-components';
+
+export default {
+  title: 'Components/Banner',
+  component: BannerComponent,
+  args: {
+    content: 'This is a test alert',
+  },
+} as Meta;
+
+export const Primary: Story = ({ content }) => ({
+  template: `<banner>${content}</banner>`,
+});
